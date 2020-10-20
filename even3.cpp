06d79122114e1a02b4c4aaddb3e551388b71268a
smityvan <59680612+smityvan@users.noreply.github.com>
2020-10-20 10:31:34 +0530

    Update even3.cpp

--- even3.cpp
+++ even3.cpp
@@ -1,53 +1,89 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 fstream fp;
-int count1=-1,count2=-1;
+int n1, n2;
 class lists
 {
-	public:
-		string l1[100],l2[100];
-		void merge()
+public:
+	string l1[100], l2[100];
+	void merge()
+	{
+		int i1 = 0, i2 = 0;
+		while (i1 < n1 && i2 < n2)
+		{
+			if (l1[i1] == l2[i2])
 			{
-				int i1=0,i2=0;
-				while(i1<count1&&i2<count2)
-				{
-				if(l1[i1]==l2[i2])
-				{
-					cout<<l1[i1++]<<endl;
-						i2++;
-				}
-				else if(l1[i1]<l2[i2])
-					cout<<l1[i1++]<<endl;
-					
-				else if(l1[i1]>l2[i2])
-					cout<<l2[i2++]<<endl;
-	
-				}
-					while(i2<count2)
-					cout<<l2[i2++]<<endl;
-					while(i1<count1)
-					cout<<l1[i1++]<<endl;
-			}	
-}list;	
+				cout << l1[i1++] << endl;
+				i2++;
+			}
+			else if (l1[i1] < l2[i2])
+				cout << l1[i1++] << endl;
+
+			else if (l1[i1] > l2[i2])
+				cout << l2[i2++] << endl;
+		}
+		if (i1 >= n1)
+		{
+			while (i2 < n2)
+				cout << l2[i2++] << endl;
+		}
+		if (i2 >= n2)
+		{
+			while (i1 < n1)
+				cout << l1[i1++] << endl;
+		}
+	}
+};
 int main()
-{	
-	fp.open("st1.txt",ios::in);
+{
 	string name;
-	while(fp)
+	string ele;
+	lists list;
+
+	fp.open("st1.txt", ios::out); //reading contents to file 1
+	cout << "Enter the size list 1 : ";
+	cin >> n1;
+	cout << "Enter the list elements :" << endl;
+	for (int i = 0; i < n1; i++)
 	{
-		getline(fp,name,'\n');
-		list.l1[++count1]=name;
+		cin >> ele;
+		fp << ele << endl;
 	}
-	fp.close();	
-	fp.open("st2.txt",ios::in);
-	while(fp)
+	fp.close();
+
+	fp.open("st2.txt", ios::out); //reading the contents of file 2
+	cout << "Enter the size list 2: ";
+	cin >> n2;
+
+	cout << "Enter the list elements :" << endl;
+
+	for (int i = 0; i < n2; i++)
 	{
-		getline(fp,name,'\n');
-		list.l2[++count2]=name;
+		cin >> ele;
+		fp << ele << endl;
 	}
-	fp.close();	
+	fp.close();
+
+	fp.open("st1.txt", ios::in);
+	int i = -1;
+	while (fp)
+	{
+		getline(fp, name, '\n');
+		list.l1[++i] = name;
+	}
+	fp.close();
+
+	fp.open("st2.txt", ios::in);
+	i = -1;
+	while (fp)
+	{
+		getline(fp, name, '\n');
+		list.l2[++i] = name;
+	}
+	fp.close();
+
 	list.merge();
 	return 0;
 }
